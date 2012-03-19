8a73934506e0bbad8a07f8bff0dcf645baead869
Tim Baumann <tim@timbaumann.info>
2012-03-19 16:26:29 +0100

    Use Data.Text

--- OperationalTransformation.hs
+++ OperationalTransformation.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module OperationalTransformation
   ( Action(..)
   , Operation
@@ -6,12 +8,13 @@ module OperationalTransformation
   , xform
   ) where
 
-import Data.List (isPrefixOf)
+import qualified Data.Text as T
+import Data.Monoid (mappend)
 import Control.Arrow (first, second, (***))
 
 data Action = Skip Int
-            | Insert String
-            | Delete String
+            | Insert T.Text
+            | Delete T.Text
             deriving (Show)
 
 type Operation = [Action]
@@ -26,52 +29,55 @@ type Operation = [Action]
 --simplify (x:xs) = x:(simplify xs)
 --simplify [] = []
 
+haveSamePrefix :: T.Text -> T.Text -> Bool
+haveSamePrefix a b = if T.length a < T.length b then a `T.isPrefixOf` b else b `T.isPrefixOf` a
+
 merge :: Operation -> Operation -> Operation
 merge [] [] = []
 merge ((Skip n):as) ((Skip m):bs) | n == m = (Skip n):(merge as bs)
                                   | n  < m = (Skip n):(merge as ((Skip (m-n)):bs))
                                   | n  > m = (Skip m):(merge ((Skip (n-m)):as) bs)
 merge ((Skip n):as) ((Insert b):bs) = (Insert b):(merge ((Skip n):as) bs)
-merge ((Skip n):as) ((Delete b):bs) | n == length b = (Delete b):(merge as bs)
-                                    | n  < length b = let (before, after) = splitAt n b in (Delete before):(merge as ((Delete after):bs))
-                                    | n  > length b = (Delete b):(merge ((Skip (n-(length b))):as) bs)
-merge ((Insert a):as) ((Skip m):bs) | length a == m = (Insert a):(merge as bs)
-                                    | length a  < m = (Insert a):(merge as ((Skip (m - length a)):bs))
-                                    | length a  > m = let (before, after) = splitAt m a in (Insert before):(merge ((Insert after):as) bs)
+merge ((Skip n):as) ((Delete b):bs) | n == T.length b = (Delete b):(merge as bs)
+                                    | n  < T.length b = let (before, after) = T.splitAt n b in (Delete before):(merge as ((Delete after):bs))
+                                    | otherwise       = (Delete b):(merge ((Skip (n-(T.length b))):as) bs)
+merge ((Insert a):as) ((Skip m):bs) | T.length a == m = (Insert a):(merge as bs)
+                                    | T.length a  < m = (Insert a):(merge as ((Skip (m - T.length a)):bs))
+                                    | otherwise       = let (before, after) = T.splitAt m a in (Insert before):(merge ((Insert after):as) bs)
 merge ((Insert a):as) ((Insert b):bs) = (Insert b):(merge ((Insert a):as) bs)
-merge ((Insert a):as) ((Delete b):bs) | not . all id $ zipWith (==) a b = error "delete must delete what has been inserted before"
-                                      | length a == length b = merge as bs
-                                      | length a  < length b = merge as ((Delete (drop (length a) b)):bs)
-                                      | length a  > length b = merge ((Insert (drop (length b) a)):as) bs
+merge ((Insert a):as) ((Delete b):bs) | not (haveSamePrefix a b) = error "delete must delete what has been inserted before"
+                                      | T.length a == T.length b = merge as bs
+                                      | T.length a  < T.length b = merge as ((Delete (T.drop (T.length a) b)):bs)
+                                      | otherwise                = merge ((Insert (T.drop (T.length b) a)):as) bs
 merge ((Delete a):as) ((Skip m):bs) = (Delete a):(merge as ((Skip m):bs))
 merge ((Delete a):as) ((Insert b):bs) = (Delete a):(Insert b):(merge as bs)
 merge ((Delete a):as) ((Delete b):bs) = (Delete a):(merge as ((Delete b):bs))
 merge ((Delete a):as) [] = (Delete a):(merge as [])
 merge [] ((Insert b):bs) = (Insert b):(merge [] bs)
 
-apply :: Operation -> String -> String
-apply ((Skip n):xs) s = let (before, after) = splitAt n s in if length before == n then before ++ apply xs after else error "does not match"
-apply ((Insert i):xs) s = i ++ apply xs s
-apply ((Delete d):xs) s = if d `isPrefixOf` s then apply xs (drop (length d) s) else error "does not match"
+apply :: Operation -> T.Text -> T.Text
+apply ((Skip n):xs) s = let (before, after) = T.splitAt n s in if T.length before == n then before `mappend` apply xs after else error "does not match"
+apply ((Insert i):xs) s = i `mappend` apply xs s
+apply ((Delete d):xs) s = if d `T.isPrefixOf` s then apply xs (T.drop (T.length d) s) else error "does not match"
 apply [] "" = ""
 
 xform :: Operation -> Operation -> (Operation, Operation)
 xform [] [] = ([], [])
-xform ((Insert a):as) bs = (***) ((Insert a):) ((Skip $ length a):) $ xform as bs
-xform as ((Insert b):bs) = (***) ((Skip $ length b):) ((Insert b):) $ xform as bs
+xform ((Insert a):as) bs = (***) ((Insert a):) ((Skip $ T.length a):) $ xform as bs
+xform as ((Insert b):bs) = (***) ((Skip $ T.length b):) ((Insert b):) $ xform as bs
 xform ((Skip n):as) ((Skip m):bs) | n == m = both ((Skip n):) $ xform as bs
                                   | n  < m = both ((Skip n):) $ xform as ((Skip (m-n)):bs)
                                   | n  > m = both ((Skip m):) $ xform ((Skip (n-m)):as) bs
-xform ((Delete a):as) ((Delete b):bs) | not . all id $ zipWith (==) a b = error "two transformations for the same document must delete the same string when they delete something at the same index"
-                                      | length a == length b = xform as bs
-                                      | length a  < length b = xform as ((Delete $ drop (length a) b):bs)
-                                      | length a  > length b = xform ((Delete $ drop (length b) a):as) bs
-xform ((Skip n):as) ((Delete b):bs) | n == length b = second ((Delete b):) $ xform as bs
-                                    | n  < length b = let (before, after) = splitAt n b in second ((Delete before):) $ xform as ((Delete after):bs)
-                                    | n  > length b = second ((Delete b):) $ xform ((Skip (n-(length b))):as) bs
-xform ((Delete a):as) ((Skip m):bs) | length a == m = first ((Delete a):) $ xform as bs
-                                    | length a  < m = first ((Delete a):) $ xform as ((Skip (m-(length a))):bs)
-                                    | length a  > m = let (before, after) = splitAt m a in first ((Delete before):) $ xform ((Delete after):as) bs
+xform ((Delete a):as) ((Delete b):bs) | not (haveSamePrefix a b) = error "two transformations for the same document must delete the same string when they delete something at the same index"
+                                      | T.length a == T.length b = xform as bs
+                                      | T.length a  < T.length b = xform as ((Delete $ T.drop (T.length a) b):bs)
+                                      | otherwise                = xform ((Delete $ T.drop (T.length b) a):as) bs
+xform ((Skip n):as) ((Delete b):bs) | n == T.length b = second ((Delete b):) $ xform as bs
+                                    | n  < T.length b = let (before, after) = T.splitAt n b in second ((Delete before):) $ xform as ((Delete after):bs)
+                                    | otherwise       = second ((Delete b):) $ xform ((Skip (n-(T.length b))):as) bs
+xform ((Delete a):as) ((Skip m):bs) | T.length a == m = first ((Delete a):) $ xform as bs
+                                    | T.length a  < m = first ((Delete a):) $ xform as ((Skip (m-(T.length a))):bs)
+                                    | otherwise       = let (before, after) = T.splitAt m a in first ((Delete before):) $ xform ((Delete after):as) bs
 
 both :: (a -> b) -> (a, a) -> (b, b)
 both f = (***) f f
--- TestOperationalTransformation.hs
+++ TestOperationalTransformation.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import OperationalTransformation
 
 import Test.QuickCheck
+import qualified Data.Text as T
+import Data.String (fromString)
 
 import Control.Monad (liftM, liftM2)
 
@@ -10,61 +14,64 @@ instance Arbitrary Action where
                     , liftM Delete arbitrary
                     ]
 
-data StringOperationPair = SOP String Operation deriving (Show)
+data TextOperationPair = SOP T.Text Operation deriving (Show)
 
-instance Arbitrary StringOperationPair where
+instance Arbitrary TextOperationPair where
   arbitrary = do
-    string <- arbitrary
-    operation <- arbitraryOperation string
-    return $ SOP string operation
+    text <- arbitrary
+    operation <- arbitraryOperation text
+    return $ SOP text operation
 
 -- | has at least 1 element
 arbitraryList :: (Arbitrary a) => Gen [a]
 arbitraryList = liftM2 (:) arbitrary arbitrary
 
-arbitraryOperation :: String -> Gen Operation
-arbitraryOperation "" = oneof [return [], liftM ((:[]) . Insert) arbitraryList]
+arbitraryOperation :: T.Text -> Gen Operation
+arbitraryOperation "" = oneof [return [], liftM ((:[]) . Insert . fromString) arbitraryList]
 arbitraryOperation s = do
-  len <- choose (1, (min 10 (length s)))
-  oneof [ liftM ((Skip len):) $ arbitraryOperation (drop len s)
-        , do s2 <- liftM2 (:) arbitrary arbitrary -- make sure that the string a length of at least one
+  len <- choose (1, (min 10 (T.length s)))
+  oneof [ liftM ((Skip len):) $ arbitraryOperation (T.drop len s)
+        , do s2 <- liftM2 ((.) (.) (.) fromString (:)) arbitrary arbitrary -- make sure that the text has a length of at least one
              next <- arbitraryOperation s
              return $ (Insert s2):next
-        , do let (before, after) = splitAt len s
+        , do let (before, after) = T.splitAt len s
              next <- arbitraryOperation after
              return $ (Delete before):next
         ]
 
-data StringOperationTriple = SOT String Operation Operation deriving (Show)
+data TextOperationTriple = SOT T.Text Operation Operation deriving (Show)
 
-instance Arbitrary StringOperationTriple where
+instance Arbitrary TextOperationTriple where
   arbitrary = do
-    (SOP string operation1) <- arbitrary
-    operation2 <- arbitraryOperation (apply operation1 string)
-    return $ SOT string operation1 operation2
+    (SOP text operation1) <- arbitrary
+    operation2 <- arbitraryOperation (apply operation1 text)
+    return $ SOT text operation1 operation2
 
-data StringConcurrentOperationTriple = SCOT String Operation Operation deriving (Show)
+data TextConcurrentOperationTriple = SCOT T.Text Operation Operation deriving (Show)
 
-instance Arbitrary StringConcurrentOperationTriple where
+instance Arbitrary T.Text where
+  arbitrary = liftM fromString arbitrary
+
+instance Arbitrary TextConcurrentOperationTriple where
   arbitrary = do
-    string <- arbitrary
-    operationA <- arbitraryOperation string
-    operationB <- arbitraryOperation string
-    return $ SCOT string operationA operationB
+    text <- arbitrary
+    operationA <- arbitraryOperation text
+    operationB <- arbitraryOperation text
+    return $ SCOT text operationA operationB
 
 deltaLength :: Operation -> Int
 deltaLength = sum . map len
   where len (Skip _)   = 0
-        len (Insert i) = length i
-        len (Delete d) = -(length d)
+        len (Insert i) = T.length i
+        len (Delete d) = -(T.length d)
 
-prop_length :: StringOperationPair -> Bool
-prop_length (SOP str op) = length (apply op str) == length str + deltaLength op
+prop_length :: TextOperationPair -> Bool
+prop_length (SOP str op) = T.length (apply op str) == T.length str + deltaLength op
 
-prop_merge_length :: StringOperationTriple -> Bool
+prop_merge_length :: TextOperationTriple -> Bool
 prop_merge_length (SOT _ a b) = deltaLength a + deltaLength b == deltaLength (merge a b)
 
-prop_merge_well_formed :: StringOperationTriple -> Bool
+prop_merge_well_formed :: TextOperationTriple -> Bool
 prop_merge_well_formed (SOT _ a b) = wellFormed $ merge a b
 
 wellFormed :: Operation -> Bool
@@ -73,14 +80,14 @@ wellFormed = all (not . nullLength)
         nullLength (Insert i) = i == ""
         nullLength (Delete d) = d == ""
 
-prop_merge_apply :: StringOperationTriple -> Bool
+prop_merge_apply :: TextOperationTriple -> Bool
 prop_merge_apply (SOT s a b) = apply b (apply a s) == apply (merge a b) s
 
-prop_xform_length :: StringConcurrentOperationTriple -> Bool
+prop_xform_length :: TextConcurrentOperationTriple -> Bool
 prop_xform_length (SCOT _ a b) = let (a', b') = xform a b
                                  in deltaLength a + deltaLength b' == deltaLength b + deltaLength a'
 
-prop_xform_apply :: StringConcurrentOperationTriple -> Bool
+prop_xform_apply :: TextConcurrentOperationTriple -> Bool
 prop_xform_apply (SCOT s a b) = let (a', b') = xform a b
                                 in (apply b' . apply a $ s) == (apply a' . apply b $ s)
 
